ee61cb7b0bf497694d6bc329b32f9be69f951212
Cody <codylau6233@hotmail.com>
2023-11-05 14:41:14 -0800

    Add sumItems function in index.js: -Pass unit testing

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  let sum = 0;
+
+  for (const num of array) {
+    if (Array.isArray(num)) {
+      // if there's a sub array, sum += recursion
+      sum += sumItems(num);
+    } else {
+      sum += num;
+    }
+  }
+  
+  return sum;
+};
 
 module.exports = sumItems;
